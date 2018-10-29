9082de9a0970e99e882de4c7fa63f2a9ee9f0e71
K514 <lighting12@naver.com>
2018-10-30 01:49:46 +0900

    Add Singleton Template

---
+++ System/Interface/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace K514
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance;
+        public static T GetInstance {
+            get
+            {
+                if (Instance == null)
+                {
+                    Instance = FindObjectOfType<T>();
+                }
+                return Instance;
+            }
+        }
+    }
+}
